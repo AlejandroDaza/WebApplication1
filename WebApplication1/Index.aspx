f48f5229a12f4ffcacb8de45f5afb98996d55d28
AlejandroDaza <dazila@gmail.com>
2017-07-23 21:21:48 +0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Index.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+       <style>
+    body {
+        position: relative; 
+    }
+
+   #mystuff {
+        position: absolute;
+        z-index: 10;
+        border-color: red;
+        background-color:aquamarine;
+        width: 100%;
+    }
+
+   #theirstuff {
+        position: relative;
+        z-index: 1;
+        width:100%;
+        align-content:center;
+    }
+   </style>
+    <script src = 'https://code.jquery.com/jquery-3.1.1.min.js'></script> 
+   <script type="text/javascript" src="jquery.facedetection.min.js"></script>
+    <title>Web</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <div id="mystuff"  >
+         Browse the Web:<asp:TextBox ID="txtUrl" runat="server" Width="300"/>
+        <asp:Button ID="btnSubmit" runat="server" Text="Get the Webpage" Width="120px" OnClick="Gethtml" />  
+        <!-- <a id ='try-it' href ='#'>-->
+             <button class="mybuttons" id ='try-it'>Blur faces</button>
+        <!-- </a>-->
+        <br />
+        <i>Enter a URL starting with <code>http://</code></i><br />  
+    </div> 
+    </form>
+     <div id="theirstuff">
+             <asp:Label ID="lblHTML" runat="server" Width="100%" ></asp:Label>            
+        </div>  
+</body>
+</html>
---
+++ WebApplication1/Index.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+using System.Net;
+using System.Text;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Gethtml(object sender, EventArgs e)
+        {
+            WebClient objWebClient = new WebClient();
+            try
+            {
+                //get the information from the web site
+                UTF8Encoding objUTF8 = new UTF8Encoding();
+                var html = new HtmlDocument();
+                html.LoadHtml(objUTF8.GetString(objWebClient.DownloadData(txtUrl.Text)));
+                var root = html.DocumentNode;
+
+                //get all the images form the website using tag img       
+                string nameImage = "mypictureA";
+                var position = 1;
+                //create a second webclient for get the images
+                WebClient webClient = new WebClient();
+                Boolean imagenproblem = false;
+                foreach (HtmlNode link in html.DocumentNode.SelectNodes("//img"))
+                {
+                    //get and safe all images
+                    string filepath = Server.MapPath(nameImage + position + ".jpeg");
+
+                    try
+                    {
+                        //trying to get the imagen form web
+                        HtmlAttribute src = link.Attributes["src"];
+                        string imageUrl = src.Value;
+                        webClient.DownloadFile(imageUrl, filepath);
+                    }
+                    catch (Exception)
+                    {
+                        
+                        //just jump this pic if can't get it
+                        imagenproblem = true;
+                    }
+                    //if can't get hte image set with a defaul image
+                    if (imagenproblem)
+                    {
+                        //changing the id for the image for can be recognized with the script
+                        link.SetAttributeValue("id", nameImage + position);
+                        //change the image for the local version
+                        link.SetAttributeValue("src", "mypictureA0.jpeg");
+                        position++;
+                        imagenproblem = false;
+                    }
+                    else
+                    {
+                        //changing the id for the image for can be recognized with the script
+                        link.SetAttributeValue("id", nameImage + position);
+                        //change the image for the local version
+                        link.SetAttributeValue("src", nameImage + position + ".jpeg");
+                        position++;
+                    }
+
+                }
+
+                //creating a html style that will be used in face recognition script
+                string newContentStyle = "<style> .myface {  position: absolute;  border: 2px solid #FFF; } </ style >";
+
+                //creating a new node with the style
+                HtmlNode newNodeStyle = HtmlNode.CreateNode(newContentStyle);
+
+                // Get header node of the page
+                HtmlNode header = html.DocumentNode.SelectSingleNode("//head");
+                // injecting the html into the original page      
+                header.AppendChild(newNodeStyle);
+
+                //creating the script for blur faces in every pic
+                string newContentScript = "<script src='jquery.facedetection.min.js'></script> ";
+                string fullscriptFaces = " <script>$(function() { 'use strict';  $('#try-it').click(function(e) {  e.preventDefault();";
+                for (var numpic = 1; numpic < position; numpic++)
+                {
+                    fullscriptFaces = fullscriptFaces + "$('#mypictureA" + numpic + "').faceDetection({ complete: function(faces) {for (var i = 0; i < faces.length; i++)";
+                    fullscriptFaces = fullscriptFaces + "{window.alert('Face detected!'); $('<div>', {  'class':'myface','position': 'absolute', 'border': '2px solid #FFF',  'css': { 'position': 'absolute',  'left':     faces[i].x * faces[i].scaleX + 'px',";
+                    fullscriptFaces = fullscriptFaces + " 'top':      faces[i].y * faces[i].scaleY + 'px',  'width':    faces[i].width * faces[i].scaleX + 'px',  'height':   faces[i].height * faces[i].scaleY + 'px',  'filter': 'blur(10px)','-webkit-filter': 'blur(10px)','-moz-filter': 'blur(10px)', '-o-filter': 'blur(10px)', '-ms-filter': 'blur(10px)', 'z-index': '200'  ";
+                    fullscriptFaces = fullscriptFaces + "} })  .insertAfter(this); } },  error: function(code, message) {  alert('Error: ' + message); } });";
+                }
+                fullscriptFaces = fullscriptFaces + "}); }); </script> ";
+
+
+                // Get body node of the webpage
+                HtmlNode body = html.DocumentNode.SelectSingleNode("//body");
+                // Creating a node with the script for blur faces
+                HtmlNode newNodeScriptFaces = HtmlNode.CreateNode(fullscriptFaces);
+                HtmlNode newNodeScript = HtmlNode.CreateNode(newContentScript);
+                // injecting the html in the original webpage
+                body.AppendChild(newNodeScript);
+                body.AppendChild(newNodeScriptFaces);
+                //load the page in the html
+                lblHTML.Text = html.DocumentNode.OuterHtml;
+            }
+            catch (Exception)
+            {
+                //do nothing if the page can't be loaded
+            }
+
+        }
+
+    }
+}
